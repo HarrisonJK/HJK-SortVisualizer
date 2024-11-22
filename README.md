1c3f141e11a7eb0035e0341dab6d57e5cbabca07
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2024-11-21 21:52:53 -0500

    added comments for the visualizer js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HJK-SortVisualizer
+
 A sorting algorithm visualizer I built with HTML, CSS, and JavaScript to deepen my understanding of sorting algorithms. I hope it helps you learn something too!
+
+https://harrisonhjk.com/HJK-SortVisualizer/
--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -1,14 +1,14 @@
 function generateRandomBars() {
   const barContainer = document.getElementById("bar-container");
-  barContainer.innerHTML = "";
+  barContainer.innerHTML = ""; // Clear any existing bars
 
-  const numberOfBars = 30;
+  const numberOfBars = 30; // Number of bars to generate (will be variable in the future)
   for (let i = 0; i < numberOfBars; i++) {
-    const barHeight = Math.floor(Math.random() * 100) + 10;
+    const barHeight = Math.floor(Math.random() * 100) + 10; // Random height
     const bar = document.createElement("div");
     bar.classList.add("bar");
     bar.style.height = `${barHeight}%`;
-    barContainer.appendChild(bar);
+    barContainer.appendChild(bar); // Add the bars to the container
   }
 }
 
@@ -19,5 +19,5 @@ function startSorting() {
     return;
   }
 
-  console.log(`Starting ${algorithm.textContent.trim()}...`);
+  console.log(`Starting ${algorithm.textContent.trim()}...`); // Log the selected algorithm
 }
